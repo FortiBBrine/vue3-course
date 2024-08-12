73998b51b3db14ff1c46604860ba9d3ce9faaecd
Aleksandr  Steblevets <fortibrinelike@gmail.com>
2024-08-12 13:57:52 +0300

    v-on:click -> @click

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <div>
-      <button v-on:click="addLike">Like</button>
-      <button v-on:click="addDislike">Dislike</button>
+      <button @click="addLike">Like</button>
+      <button @click="addDislike">Dislike</button>
     </div>
     <div>Кол-во лайков <strong>{{ likes }}</strong> </div>
     <div>Кол-во дизлайков <strong>{{ dislikes }}</strong> </div>
