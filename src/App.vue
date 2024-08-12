50daa0400aefce5261079ba963a8c02439ae4465
FortiBrine <FortiBrine@users.noreply.github.com>
2024-08-12 16:37:34 +0300

    vue if else

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
 <!--    <post-list v-bind:posts="posts"/>-->
     <post-list
         :posts="posts"
+        @remove="removePost"
     />
 
   </div>
@@ -33,6 +34,9 @@ export default {
     createPost(post) {
       this.posts.push(post);
     },
+    removePost(post) {
+      this.posts = this.posts.filter(p => p.id !== post.id)
+    }
   }
 }
 </script>
